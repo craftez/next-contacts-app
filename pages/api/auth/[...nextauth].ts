213d425766d11c7574967539220cfb8aa9ed1fa1
Eduardo Zamora <eduzambl@gmail.com>
2021-10-28 11:12:12 -0600

    feat: update next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,44 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
-async function refreshAccessToken(token: any) {
-  try {
-    const url = `https://oauth2.googleapis.com/token?${new URLSearchParams({
-      client_id: process.env.GOOGLE_ID!,
-      client_secret: process.env.GOOGLE_SECRET!,
-      grant_type: "refresh_token",
-      refresh_token: token.refreshToken,
-    })}`;
-
-    const response = await fetch(url, {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      method: "POST",
-    });
-
-    const refreshedTokens = await response.json();
-
-    if (!response.ok) {
-      throw refreshedTokens;
-    }
-
-    return {
-      ...token,
-      accessToken: refreshedTokens.access_token,
-      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
-      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fallback to old refresh tokens
-    };
-  } catch (error) {
-    console.log("¯_(ツ)_/¯", error);
-
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    };
-  }
-}
-
 export default NextAuth({
   providers: [
     Providers.Google({
@@ -51,30 +13,14 @@ export default NextAuth({
   secret: process.env.APP_SECRET,
   callbacks: {
     async session(session, token: any) {
-      session.id = token.user.id;
-      session.user = token.user;
-      console.log("___ session:", session, token);
+      session.id = token.sub;
       return session;
     },
     async jwt(token, user, account) {
-      console.log("___ jwt:", token, user, account);
-      // Initial sign in
-      if (account && user) {
-        return {
-          accessToken: account.accessToken,
-          accessTokenExpires: Date.now() + account?.expires_in! * 1000,
-          refreshToken: account.refreshToken,
-          user,
-        };
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
       }
-
-      // Return previous token if the access token has not expired yet
-      if (Date.now() < (token as any).accessTokenExpires) {
-        return token;
-      }
-
-      // Access token has expired, try to update it
-      return refreshAccessToken(token);
+      return token;
     },
   },
 });
